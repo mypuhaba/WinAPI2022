f58285d61967e664f389c694d371cd250e9aca32
mypuhaba <mypuhaba@gmail.com>
2022-05-21 21:16:09 +0900

    5-1 모두 구현

--- Trainings/5-1/5-1.cpp
+++ Trainings/5-1/5-1.cpp
@@ -45,25 +45,34 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR ipszCmdPr
 LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
 {
 	PAINTSTRUCT ps;
-	static int xPos,yPos, nW, nH, w, h, zw, zh; // xPos/yPos: DC에 그릴 위치
-	//nW/nH: 대상 사각형, w/h: 소스 사각형
 	static CImage img;
-	HDC hdc, memdc;
-	static RECT rect;
+	static HDC hdc, memdc;
+	static RECT rect; // 윈도우 사각형
+	static int xPos, yPos, nW, nH, sw, sh, zw, zh, mx, my;
+	// xPos/yPos: DC에 그릴 위치
+	// nW/nH: 그릴 사각형 크기, sw/sh: 소스 이미지 크기
+	// zw/zh: 크기 변경, mx/my: 마우스 위치
 	static HBITMAP hBitmap;
-	static DWORD dwRop;
+	static DWORD dwRop; // 소스 출력
+	static int div; // 2: 2x2분할, 3: 3x3분할
+	static RECT selection;
+	static bool select[3][3];
+	static int i, j;
+	static bool click;
 
 	switch (iMsg)
 	{
 	case WM_CREATE:
+		dwRop = SRCCOPY;
 		img.Load(TEXT("image1.jpg"));
 		GetClientRect(hwnd, &rect);
-		xPos = 0; yPos = 0;
-		w = img.GetWidth();
-		h = img.GetHeight();
-		nW = w; nH = h;
+		sw = img.GetWidth();
+		sh = img.GetHeight();
+		xPos = 0;
+		nW = sw; nH = sh;
 		zw = 0; zh = 0;
-		dwRop = SRCCOPY;
+		div = 1;
+		click = false;
 		break;
 
 	case WM_PAINT:
@@ -71,11 +80,209 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
 		hBitmap = CreateCompatibleBitmap(hdc, rect.right, rect.bottom);
 		memdc = CreateCompatibleDC(hdc);
 		(HBITMAP)SelectObject(memdc, hBitmap);
-		img.Draw(memdc, 0, 0, nW, nH, 0, 0, w, h);
-		//StretchBlt(hdc, 0, 0, nW + zw, nH + zh, memdc, 0, 0, w, h, SRCCOPY);
-		BitBlt(hdc, xPos, yPos, nW, nH, memdc, 0, 0, dwRop);
-		DeleteObject(hBitmap);
+		HBRUSH hBrush;
+		hBrush = CreateSolidBrush(RGB(255, 0, 0));
+		(HBRUSH)SelectObject(hdc, hBrush);
+		
+		if (div == 2)
+		{
+			img.Draw(memdc, 0, 0, nW, nH, 0, 0, sw, sh);
+			if (click)
+			{
+				selection = { j * nW + xPos, i * nH, j * nW + nW + xPos, i * nH + nH };
+				if (select[0][0])
+				{
+					BitBlt(hdc, nW, 0, nW, nH, memdc, 0, 0, NOTSRCCOPY); // 2
+					BitBlt(hdc, xPos, 0, nW, nH, memdc, 0, 0, SRCCOPY); // 3
+					BitBlt(hdc, 0, nH, nW, nH, memdc, 0, 0, NOTSRCCOPY); // 4
+					BitBlt(hdc, nW, nH, nW, nH, memdc, 0, 0, SRCCOPY); // 1
+				}
+				else if (select[0][1])
+				{
+					BitBlt(hdc, 0, 0, nW, nH, memdc, 0, 0, SRCCOPY); // 1
+					BitBlt(hdc, xPos + nW, 0, nW, nH, memdc, 0, 0, NOTSRCCOPY); // 3
+					BitBlt(hdc, 0, nH, nW, nH, memdc, 0, 0, NOTSRCCOPY); // 4
+					BitBlt(hdc, nW, nH, nW, nH, memdc, 0, 0, SRCCOPY); // 2
+				}
+				else if (select[1][0])
+				{
+					BitBlt(hdc, 0, 0, nW, nH, memdc, 0, 0, SRCCOPY); // 1
+					BitBlt(hdc, nW, 0, nW, nH, memdc, 0, 0, NOTSRCCOPY); // 2
+					BitBlt(hdc, nW, nH, nW, nH, memdc, 0, 0, SRCCOPY); // 4
+					BitBlt(hdc, xPos, nH, nW, nH, memdc, 0, 0, NOTSRCCOPY); // 3
+				}
+				else if (select[1][1])
+				{
+					BitBlt(hdc, 0, 0, nW, nH, memdc, 0, 0, SRCCOPY); // 1
+					BitBlt(hdc, nW, 0, nW, nH, memdc, 0, 0, NOTSRCCOPY); // 2
+					BitBlt(hdc, 0, nH, nW, nH, memdc, 0, 0, NOTSRCCOPY); // 3
+					BitBlt(hdc, nW + xPos, nH, nW, nH, memdc, 0, 0, SRCCOPY); // 4
+				}
+				FrameRect(hdc, &selection, hBrush);
+				DeleteObject(hBitmap);
+			}
+			else
+			{
+				BitBlt(hdc, 0, 0, nW, nH, memdc, 0, 0, SRCCOPY); // 1
+				BitBlt(hdc, nW, 0, nW, nH, memdc, 0, 0, NOTSRCCOPY); // 2
+				BitBlt(hdc, 0, nH, nW, nH, memdc, 0, 0, NOTSRCCOPY); // 3
+				BitBlt(hdc, nW, nH, nW, nH, memdc, 0, 0, SRCCOPY); // 4
+			}
+		}
+		else if (div == 3)
+		{
+				img.Draw(memdc, 0, 0, nW, nH, 0, 0, sw, sh);
+				if (click)
+				{
+					selection = { j * nW + xPos, i * nH, j * nW + nW + xPos, i * nH + nH };
+					if (select[0][0])
+					{
+						BitBlt(hdc, nW, 0, nW, nH, memdc, 0, 0, SRCCOPY); // 2
+						BitBlt(hdc, nW * 2, 0, nW, nH, memdc, 0, 0, NOTSRCCOPY);  // 3
+						BitBlt(hdc, 0, 0 + nH, nW, nH, memdc, 0, 0, SRCCOPY); // 4
+						BitBlt(hdc, 0 + nW, 0 + nH, nW, nH, memdc, 0, 0, NOTSRCCOPY); // 5
+						BitBlt(hdc, 0 + nW * 2, 0 + nH, nW, nH, memdc, 0, 0, SRCCOPY); // 6
+						BitBlt(hdc, 0, 0 + 2 * nH, nW, nH, memdc, 0, 0, NOTSRCCOPY); // 7
+						BitBlt(hdc, 0 + nW, 0 + nH * 2, nW, nH, memdc, 0, 0, SRCCOPY); // 8
+						BitBlt(hdc, 0 + nW * 2, 0 + nH * 2, nW, nH, memdc, 0, 0, NOTSRCCOPY); // 9
+						BitBlt(hdc, xPos, 0, nW, nH, memdc, 0, 0, NOTSRCCOPY); // 1
+					}
+					else if (select[0][1])
+					{
+						BitBlt(hdc, 0, 0, nW, nH, memdc, 0, 0, NOTSRCCOPY); // 1
+						BitBlt(hdc, nW * 2, 0, nW, nH, memdc, 0, 0, NOTSRCCOPY);  // 3
+						BitBlt(hdc, 0, 0 + nH, nW, nH, memdc, 0, 0, SRCCOPY); // 4
+						BitBlt(hdc, 0 + nW, 0 + nH, nW, nH, memdc, 0, 0, NOTSRCCOPY); // 5
+						BitBlt(hdc, 0 + nW * 2, 0 + nH, nW, nH, memdc, 0, 0, SRCCOPY); // 6
+						BitBlt(hdc, 0, 0 + 2 * nH, nW, nH, memdc, 0, 0, NOTSRCCOPY); // 7
+						BitBlt(hdc, 0 + nW, 0 + nH * 2, nW, nH, memdc, 0, 0, SRCCOPY); // 8
+						BitBlt(hdc, 0 + nW * 2, 0 + nH * 2, nW, nH, memdc, 0, 0, NOTSRCCOPY); // 9
+						BitBlt(hdc, nW + xPos, 0, nW, nH, memdc, 0, 0, SRCCOPY); // 2
+					}
+					else if (select[0][2])
+					{
+						BitBlt(hdc, 0, 0, nW, nH, memdc, 0, 0, NOTSRCCOPY); // 1
+						BitBlt(hdc, nW, 0, nW, nH, memdc, 0, 0, SRCCOPY); // 2
+						BitBlt(hdc, 0, 0 + nH, nW, nH, memdc, 0, 0, SRCCOPY); // 4
+						BitBlt(hdc, 0 + nW, 0 + nH, nW, nH, memdc, 0, 0, NOTSRCCOPY); // 5
+						BitBlt(hdc, 0 + nW * 2, 0 + nH, nW, nH, memdc, 0, 0, SRCCOPY); // 6
+						BitBlt(hdc, 0, 0 + 2 * nH, nW, nH, memdc, 0, 0, NOTSRCCOPY); // 7
+						BitBlt(hdc, 0 + nW, 0 + nH * 2, nW, nH, memdc, 0, 0, SRCCOPY); // 8
+						BitBlt(hdc, 0 + nW * 2, 0 + nH * 2, nW, nH, memdc, 0, 0, NOTSRCCOPY); // 9
+						BitBlt(hdc, nW * 2 + xPos, 0, nW, nH, memdc, 0, 0, NOTSRCCOPY);  // 3
+					}
+					else if (select[1][0])
+					{
+						BitBlt(hdc, 0, 0, nW, nH, memdc, 0, 0, NOTSRCCOPY); // 1
+						BitBlt(hdc, nW, 0, nW, nH, memdc, 0, 0, SRCCOPY); // 2
+						BitBlt(hdc, nW * 2, 0, nW, nH, memdc, 0, 0, NOTSRCCOPY);  // 3
+						BitBlt(hdc, 0 + nW, 0 + nH, nW, nH, memdc, 0, 0, NOTSRCCOPY); // 5
+						BitBlt(hdc, 0 + nW * 2, 0 + nH, nW, nH, memdc, 0, 0, SRCCOPY); // 6
+						BitBlt(hdc, 0, 0 + 2 * nH, nW, nH, memdc, 0, 0, NOTSRCCOPY); // 7
+						BitBlt(hdc, 0 + nW, 0 + nH * 2, nW, nH, memdc, 0, 0, SRCCOPY); // 8
+						BitBlt(hdc, 0 + nW * 2, 0 + nH * 2, nW, nH, memdc, 0, 0, NOTSRCCOPY); // 9
+						BitBlt(hdc, xPos, 0 + nH, nW, nH, memdc, 0, 0, SRCCOPY); // 4
+					}
+					else if (select[1][1])
+					{
+						BitBlt(hdc, 0, 0, nW, nH, memdc, 0, 0, NOTSRCCOPY); // 1
+						BitBlt(hdc, nW, 0, nW, nH, memdc, 0, 0, SRCCOPY); // 2
+						BitBlt(hdc, nW * 2, 0, nW, nH, memdc, 0, 0, NOTSRCCOPY);  // 3
+						BitBlt(hdc, 0, 0 + nH, nW, nH, memdc, 0, 0, SRCCOPY); // 4
+						BitBlt(hdc, 0 + nW * 2, 0 + nH, nW, nH, memdc, 0, 0, SRCCOPY); // 6
+						BitBlt(hdc, 0, 0 + 2 * nH, nW, nH, memdc, 0, 0, NOTSRCCOPY); // 7
+						BitBlt(hdc, 0 + nW, 0 + nH * 2, nW, nH, memdc, 0, 0, SRCCOPY); // 8
+						BitBlt(hdc, 0 + nW * 2, 0 + nH * 2, nW, nH, memdc, 0, 0, NOTSRCCOPY); // 9
+						BitBlt(hdc, 0 + nW + xPos, 0 + nH, nW, nH, memdc, 0, 0, NOTSRCCOPY); // 5
+					}
+					else if (select[1][2])
+					{
+						BitBlt(hdc, 0, 0, nW, nH, memdc, 0, 0, NOTSRCCOPY); // 1
+						BitBlt(hdc, nW, 0, nW, nH, memdc, 0, 0, SRCCOPY); // 2
+						BitBlt(hdc, nW * 2, 0, nW, nH, memdc, 0, 0, NOTSRCCOPY);  // 3
+						BitBlt(hdc, 0, 0 + nH, nW, nH, memdc, 0, 0, SRCCOPY); // 4
+						BitBlt(hdc, 0 + nW, 0 + nH, nW, nH, memdc, 0, 0, NOTSRCCOPY); // 5
+						BitBlt(hdc, 0, 0 + 2 * nH, nW, nH, memdc, 0, 0, NOTSRCCOPY); // 7
+						BitBlt(hdc, 0 + nW, 0 + nH * 2, nW, nH, memdc, 0, 0, SRCCOPY); // 8
+						BitBlt(hdc, 0 + nW * 2, 0 + nH * 2, nW, nH, memdc, 0, 0, NOTSRCCOPY); // 9
+						BitBlt(hdc, 0 + nW * 2 + xPos, 0 + nH, nW, nH, memdc, 0, 0, SRCCOPY); // 6
+					}
+					else if (select[2][0])
+					{
+						BitBlt(hdc, xPos, 0 + 2 * nH, nW, nH, memdc, 0, 0, NOTSRCCOPY); // 7
+						BitBlt(hdc, 0, 0, nW, nH, memdc, 0, 0, NOTSRCCOPY); // 1
+						BitBlt(hdc, nW, 0, nW, nH, memdc, 0, 0, SRCCOPY); // 2
+						BitBlt(hdc, nW * 2, 0, nW, nH, memdc, 0, 0, NOTSRCCOPY);  // 3
+						BitBlt(hdc, 0, 0 + nH, nW, nH, memdc, 0, 0, SRCCOPY); // 4
+						BitBlt(hdc, 0 + nW, 0 + nH, nW, nH, memdc, 0, 0, NOTSRCCOPY); // 5
+						BitBlt(hdc, 0 + nW * 2, 0 + nH, nW, nH, memdc, 0, 0, SRCCOPY); // 6
+						BitBlt(hdc, 0 + nW, 0 + nH * 2, nW, nH, memdc, 0, 0, SRCCOPY); // 8
+						BitBlt(hdc, 0 + nW * 2, 0 + nH * 2, nW, nH, memdc, 0, 0, NOTSRCCOPY); // 9
+						
+					}
+					else if (select[2][1])
+					{
+						BitBlt(hdc, nW + xPos, 0 + nH * 2, nW, nH, memdc, 0, 0, SRCCOPY); // 8
+						BitBlt(hdc, 0, 0, nW, nH, memdc, 0, 0, NOTSRCCOPY); // 1
+						BitBlt(hdc, nW, 0, nW, nH, memdc, 0, 0, SRCCOPY); // 2
+						BitBlt(hdc, nW * 2, 0, nW, nH, memdc, 0, 0, NOTSRCCOPY);  // 3
+						BitBlt(hdc, 0, 0 + nH, nW, nH, memdc, 0, 0, SRCCOPY); // 4
+						BitBlt(hdc, 0 + nW, 0 + nH, nW, nH, memdc, 0, 0, NOTSRCCOPY); // 5
+						BitBlt(hdc, 0 + nW * 2, 0 + nH, nW, nH, memdc, 0, 0, SRCCOPY); // 6
+						BitBlt(hdc, 0, 0 + 2 * nH, nW, nH, memdc, 0, 0, NOTSRCCOPY); // 7
+						BitBlt(hdc, 0 + nW * 2, 0 + nH * 2, nW, nH, memdc, 0, 0, NOTSRCCOPY); // 9
+					
+					}
+					else if (select[2][2])
+					{
+						BitBlt(hdc, 0, 0, nW, nH, memdc, 0, 0, NOTSRCCOPY); // 1
+						BitBlt(hdc, nW, 0, nW, nH, memdc, 0, 0, SRCCOPY); // 2
+						BitBlt(hdc, nW * 2, 0, nW, nH, memdc, 0, 0, NOTSRCCOPY);  // 3
+						BitBlt(hdc, 0, 0 + nH, nW, nH, memdc, 0, 0, SRCCOPY); // 4
+						BitBlt(hdc, 0 + nW, 0 + nH, nW, nH, memdc, 0, 0, NOTSRCCOPY); // 5
+						BitBlt(hdc, 0 + nW * 2, 0 + nH, nW, nH, memdc, 0, 0, SRCCOPY); // 6
+						BitBlt(hdc, 0, 0 + 2 * nH, nW, nH, memdc, 0, 0, NOTSRCCOPY); // 7
+						BitBlt(hdc, 0 + nW, 0 + nH * 2, nW, nH, memdc, 0, 0, SRCCOPY); // 8
+						BitBlt(hdc, nW * 2 + xPos, 0 + nH * 2, nW, nH, memdc, 0, 0, NOTSRCCOPY); // 9
+					}
+					FrameRect(hdc, &selection, hBrush);
+					DeleteObject(hBitmap);
+				}
+				//else if (select[][])
+				//{
+				//	BitBlt(hdc, 0, 0, nW, nH, memdc, 0, 0, NOTSRCCOPY); // 1
+				//	BitBlt(hdc, nW, 0, nW, nH, memdc, 0, 0, SRCCOPY); // 2
+				//	BitBlt(hdc, nW * 2, 0, nW, nH, memdc, 0, 0, NOTSRCCOPY);  // 3
+				//	BitBlt(hdc, 0, 0 + nH, nW, nH, memdc, 0, 0, SRCCOPY); // 4
+				//	BitBlt(hdc, 0 + nW, 0 + nH, nW, nH, memdc, 0, 0, NOTSRCCOPY); // 5
+				//	BitBlt(hdc, 0 + nW * 2, 0 + nH, nW, nH, memdc, 0, 0, SRCCOPY); // 6
+				//	BitBlt(hdc, 0, 0 + 2 * nH, nW, nH, memdc, 0, 0, NOTSRCCOPY); // 7
+				//	BitBlt(hdc, 0 + nW, 0 + nH * 2, nW, nH, memdc, 0, 0, SRCCOPY); // 8
+				//	BitBlt(hdc, 0 + nW * 2, 0 + nH * 2, nW, nH, memdc, 0, 0, NOTSRCCOPY); // 9
+				//}
+		
+		else
+			{
+			BitBlt(hdc, xPos, 0, nW, nH, memdc, 0, 0, NOTSRCCOPY); // 1
+			BitBlt(hdc, xPos + nW, 0, nW, nH, memdc, 0, 0, SRCCOPY); // 2
+			BitBlt(hdc, xPos + nW * 2, 0, nW, nH, memdc, 0, 0, NOTSRCCOPY);  // 3
+			BitBlt(hdc, xPos, 0 + nH, nW, nH, memdc, 0, 0, SRCCOPY); // 4
+			BitBlt(hdc, xPos + nW, 0 + nH, nW, nH, memdc, 0, 0, NOTSRCCOPY); // 5
+			BitBlt(hdc, xPos + nW * 2, 0 + nH, nW, nH, memdc, 0, 0, SRCCOPY); // 6
+			BitBlt(hdc, xPos, 0 + 2 * nH, nW, nH, memdc, 0, 0, NOTSRCCOPY); // 7
+			BitBlt(hdc, xPos + nW, 0 + nH * 2, nW, nH, memdc, 0, 0, SRCCOPY); // 8
+			BitBlt(hdc, xPos + nW * 2, 0 + nH * 2, nW, nH, memdc, 0, 0, NOTSRCCOPY); // 9
+			}
+		}
+		else
+		{
+			img.Draw(memdc, 0, 0, nW + zw, nH + zh, 0, 0, sw, sh);
+			BitBlt(hdc, xPos, 0, nW + zw, nH + zh, memdc, 0, 0, dwRop);
+		}
+		DeleteObject(hBrush);
+		
 		DeleteDC(memdc);
+		
 		EndPaint(hwnd, &ps);
 		break;
 
@@ -86,13 +293,13 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
 			if (dwRop == SRCCOPY)
 				dwRop = NOTSRCCOPY;
 			else dwRop = SRCCOPY;
-			InvalidateRgn(hwnd, NULL, true);
+			InvalidateRgn(hwnd, NULL, false);
 			break;
 		case 'a':
 			if (nW == rect.right)
-				nW = w;
+				nW = sw;
 			if (nH == rect.bottom)
-				nH = h;
+				nH = sh;
 			else
 			{
 				nW = rect.right;
@@ -100,38 +307,31 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
 			}
 			InvalidateRgn(hwnd, NULL, true);
 			break;
+		case '1': // 리턴 투 초기화면
+			xPos = 0;
+			nW = sw; nH = sh;
+			zw = 0; zh = 0;
+			div = 1;
+			click = false;
+			InvalidateRgn(hwnd, NULL, true);
+			break;
 		case '2':
-			hdc = GetDC(hwnd);
-			hBitmap = CreateCompatibleBitmap(hdc, rect.right, rect.bottom);
-			memdc = CreateCompatibleDC(hdc);
-			(HBITMAP)SelectObject(memdc, hBitmap);
-			img.Draw(memdc, 0, 0, nW / 2, nH / 2, 0, 0, w, h);
-			BitBlt(hdc, xPos, yPos, nW, nH, memdc, 0, 0, SRCCOPY);
-			BitBlt(hdc, xPos + nW / 2, yPos, nW / 2, nH / 2, memdc, 0, 0, NOTSRCCOPY);
-			BitBlt(hdc, xPos, yPos + nH / 2, nW / 2, nH / 2, memdc, 0, 0, NOTSRCCOPY);
-			BitBlt(hdc, xPos + nW / 2, yPos + nH / 2, nW / 2, nH / 2, memdc, 0, 0, SRCCOPY);
-			DeleteObject(hBitmap);
-			DeleteDC(memdc);
-			ReleaseDC(hwnd, hdc);
+			xPos = 0;
+			nW = sw; nH = sh;
+			zw = 0; zh = 0;
+			click = false;
+			div = 2;
+			nW = sw / 2 + zw; nH = sh / 2 + zh;
+			InvalidateRgn(hwnd, NULL, false);
 			break;
 		case '3':
-			hdc = GetDC(hwnd);
-			hBitmap = CreateCompatibleBitmap(hdc, rect.right, rect.bottom);
-			memdc = CreateCompatibleDC(hdc);
-			(HBITMAP)SelectObject(memdc, hBitmap);
-			img.Draw(memdc, 0, 0, nW / 3, nH / 3, 0, 0, w, h);
-			BitBlt(hdc, xPos, yPos, nW / 3, nH / 3, memdc, 0, 0, NOTSRCCOPY); // 1
-			BitBlt(hdc, xPos + nW / 3, yPos, nW / 3, nH / 3, memdc, 0, 0, SRCCOPY); // 2
-			BitBlt(hdc, xPos + nW * 2 / 3, yPos, nW / 3, nH / 3, memdc, 0, 0, NOTSRCCOPY);  // 3
-			BitBlt(hdc, xPos, yPos + nH/3, nW / 3, nH / 3, memdc, 0, 0, SRCCOPY); // 4
-			BitBlt(hdc, xPos + nW / 3, yPos + nH / 3, nW / 3, nH / 3, memdc, 0, 0, NOTSRCCOPY); // 5
-			BitBlt(hdc, xPos + nW * 2 / 3, yPos + nH / 3, nW / 3, nH / 3, memdc, 0, 0, SRCCOPY); // 6
-			BitBlt(hdc, xPos, yPos + 2 * nH / 3, nW / 3, nH / 3, memdc, 0, 0, NOTSRCCOPY); // 7
-			BitBlt(hdc, xPos + nW / 3, yPos + nH * 2 / 3, nW / 3, nH / 3, memdc, 0, 0, SRCCOPY); // 8
-			BitBlt(hdc, xPos + nW * 2 / 3, yPos + nH * 2 / 3, nW / 3, nH / 3, memdc, 0, 0, NOTSRCCOPY); // 9
-			DeleteObject(hBitmap);
-			DeleteDC(memdc);
-			ReleaseDC(hwnd, hdc);
+			xPos = 0;
+			nW = sw; nH = sh;
+			zw = 0; zh = 0;
+			click = false;
+			div = 3;
+			nW = sw / 3 + zw; nH = sh / 3 + zh;
+			InvalidateRgn(hwnd, NULL, false);
 			break;
 		case 'q':
 			img.Destroy();
@@ -144,42 +344,37 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
 		switch (wParam)
 		{
 		case VK_LEFT:
-			xPos -= 10;
+			xPos -= 20;
 			InvalidateRgn(hwnd, NULL, true);
 			break;
-
 		case VK_RIGHT:
-			xPos += 10;
+			xPos += 20;
 			InvalidateRgn(hwnd, NULL, true);
 			break;
 		case VK_ADD:
-			hdc = GetDC(hwnd);
-			hBitmap = CreateCompatibleBitmap(hdc, rect.right, rect.bottom);
-			memdc = CreateCompatibleDC(hdc);
-			(HBITMAP)SelectObject(memdc, hBitmap);
-			img.Draw(memdc, 0, 0, nW, nH, 0, 0, w, h);
-			StretchBlt(hdc, 0, 0, nW + zw, nH + zh, memdc, 0, 0, w, h, WHITENESS);
-			zw += 20; zh += 20;
-			StretchBlt(hdc, 0, 0, nW + zw, nH + zh, memdc, 0, 0, w, h, SRCCOPY);
-			DeleteObject(hBitmap);
-			DeleteDC(memdc);
-			ReleaseDC(hwnd, hdc);
+			zw += sw / 50; zh += sh / 50;
+			InvalidateRgn(hwnd, NULL, true);
 			break;
 		case VK_SUBTRACT:
-			
-			hdc = GetDC(hwnd);
-			hBitmap = CreateCompatibleBitmap(hdc, rect.right, rect.bottom);
-			memdc = CreateCompatibleDC(hdc);
-			(HBITMAP)SelectObject(memdc, hBitmap);
-			img.Draw(memdc, 0, 0, nW, nH, 0, 0, w, h);
-			StretchBlt(hdc, 0, 0, nW + zw, nH + zh, memdc, 0, 0, w, h, WHITENESS);
-			zw -= 20; zh -= 20;
-			StretchBlt(hdc, 0, 0, nW + zw, nH + zh, memdc, 0, 0, w, h, SRCCOPY);
-			DeleteObject(hBitmap);
-			DeleteDC(memdc);
-			ReleaseDC(hwnd, hdc);
+			zw -= sw / 50; zh -= sh / 50;
+			InvalidateRgn(hwnd, NULL, true);
 			break;
 		}
+		break;
+
+
+	case WM_LBUTTONDOWN:
+		mx = LOWORD(lParam);
+		my = HIWORD(lParam);
+		//xPos = 0;
+		for (int i = 0; i < 3; ++i)
+			for (int j = 0; j < 3; ++j)
+				select[i][j] = false;
+		j = mx / nW; i = my / nH;
+		if (mx <= sw && my <= sh)
+		select[i][j] = true;
+		click = true;
+		InvalidateRgn(hwnd, NULL, true);
 		break;
 
 	case WM_DESTROY:
